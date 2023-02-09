0cae83faf0a29824f8ce98eb3d83137b274c87b4
Khevan Thanki <Khevanyt@gmail.com>
2023-02-10 00:01:46 +0530

    Added menus (w/ actions) for player and enemy

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ class_stats = pd.read_csv(os.path.join("CSV", "Class_Stats.csv"), header=0, enco
 enemy_stats = pd.read_csv(os.path.join("CSV", "Enemy_Stats.csv"), header=0, encoding='latin-1')
 
 
+# I - Classes
 # Player Character Class
 class PlayerChar:
     def __init__(self, name, class_num: int):
@@ -52,7 +53,7 @@ class PlayerChar:
                         self.move_name, self.move_dmg, self.curr_potionCount]
         self.stats = pd.DataFrame(data=[stats_values], columns=stats_keys)
 
-    def __str__(self): # for print(playerChar)
+    def __str__(self):  # for print(playerChar)
         str = "Name: " + self.name + ", " + "Class: " + self.class_type
         return str
 
@@ -87,7 +88,7 @@ class Enemy:
                         self.move1_name, self.move1_dmg, self.move2_name, self.move2_dmg]
         self.stats = pd.DataFrame(data=[stats_values], columns=stats_keys)
 
-    def __str__(self): # for print(playerChar)
+    def __str__(self):  # for print(playerChar)
         str = "Name: " + self.name
         return str
 
@@ -96,10 +97,7 @@ class Enemy:
 
 
 # global vars
-# to be reworked
-global wizard, archer, knight
-global sphinx, mimir, athena
-global playerChars, enemyChars
+potionHealth = 10
 
 # creating player chars
 wizard = PlayerChar("Wizard", 0)
@@ -118,6 +116,10 @@ athena = Enemy(2)
 enemyChars = [sphinx, mimir, athena]
 
 
+# === End Of I - Classes ===
+
+# II - Logging & Output Functions
+
 # display messages in log screen and adds them to text file
 def log_msg(a: str, newline):
     level = logging.INFO
@@ -129,63 +131,12 @@ def log_msg(a: str, newline):
         logging.info(" ")
 
 
-# player to enemy damage calculator
-# uses player's attack and dmg along with enemy's defense + some randomisation
-def calculateDMG_P2E(player: PlayerChar, enemy: Enemy) -> int:
-    # random factor
-    randomfac = random.uniform(1, 2)
+# === End Of II - Logging & Output Functions ===
 
-    # calculate damage
-    dmg = (player.attack * player.move_dmg * randomfac) / enemy.defense
-    return int(dmg)
+# III - Combat Related Functions
 
-
-# actually deals damage and updates the enemy's hp
-def dealDMG_P2E(dmg: int, enemy: Enemy):
-    print(enemy.name, "'s HP was: ", enemy.curr_HP)
-    enemy.curr_HP -= dmg
-    print(enemy.name, "'s HP is now: ", enemy.curr_HP)
-    print("Damage dealt was: ", dmg)
-
-
-# enemy to player damage calculator
-# uses enemy's attack and dmg along with player's defense + some randomisation
-def calculateDMG_E2P(enemy: Enemy, move_num: int, player: PlayerChar) -> int:
-    # random factor
-    randomfac = random.uniform(0.1, 0.45)
-
-    #  pick move number (thus dmg) based on move_num arg
-    if move_num == 1:
-        move_dmg = enemy_stats.iat[enemy.enemy_num, 6]  # move 1
-    elif move_num == 2:
-        move_dmg = enemy_stats.iat[enemy.enemy_num, 8]  # move 2
-
-    # calculate damage per player (regardless of move type)
-    dmg = (enemy.attack * move_dmg * randomfac) / player.defense
-    return int(dmg)
-
-
-# actually deals damage and updates the enemy's hp
-def dealDMG_E2P(player: PlayerChar, dmg: int, move_num: int):
-    if (move_num == 1):  # deal damage only to one player
-        print("This move deals damage only to ", player.name)
-        print(player.name, "'s HP was: ", player.curr_HP)
-        player.curr_HP -= dmg
-        print(player.name, "'s HP is now: ", player.curr_HP)
-        print("Damage dealt was: ", dmg)
-    elif (move_num == 2):  # deal damage to everyone
-        print("This move deals damage to all players")
-        for i in playerChars:
-            print(i.name, "'s HP was: ", i.curr_HP)
-            player.curr_HP -= dmg
-            print(i.name, "'s HP is now: ", i.curr_HP)
-            print("Damage dealt was: ", dmg)
-            print()
-
-
-# initialises things needed for combat encounter to star
+# initialises things needed for combat encounter to start
 def initialiseCombat(enemy: Enemy):
-
     # TO DO LEFT:
     # 3. check for weakness code and set damage multiplier
 
@@ -206,7 +157,7 @@ def initialiseCombat(enemy: Enemy):
         pos = i
 
         for j in range(i + 1, len(orderOfCombat)):
-            if max.speed < orderOfCombat[j].speed: # if value is smaller than current min, update it
+            if max.speed < orderOfCombat[j].speed:  # if value is smaller than current min, update it
                 max = orderOfCombat[j]
                 pos = j
 
@@ -224,7 +175,7 @@ def initialiseCombat(enemy: Enemy):
     # 2. Restore dead player's health with penalty
     for i in playerChars:
         if i.curr_HP <= 0:
-            i.curr_HP = int(i.max_HP * 0.3)   # Restored health == 30% of max
+            i.curr_HP = int(i.max_HP * 0.3)  # Restored health == 30% of max
 
     # checking if it worked
     print("The health of players after restoring health is: ")
@@ -233,8 +184,137 @@ def initialiseCombat(enemy: Enemy):
     print()
 
     # 3. check for weakness code and set damage multiplier
+    # <INCOMPLETE>
 
 
+# provides choices to players during their turn and acts on them
+def playerMenu(player: PlayerChar, enemy: Enemy):  # IN PROGRESS
+    print("Menu For: ", player.name)
+    print("Choose: \n1.Attack \n2.Guard \n3.Heal")
+    choice = int(input("Enter corresponding number: "))
+
+    # LEFT TO DO:
+    # check for damage multipliers
+
+    if choice == 1:  # 1. Attack
+        # Print move name and dmg
+        print("Move Name: ", player.move_name, ", Move DMG: ", player.move_dmg)
+
+        # Call for calculation and dealing of damage
+        dealDMG_P2E(calculateDMG_P2E(player, enemy), enemy)
+
+    elif choice == 2:  # 2. Guard
+        # HOW TF DO I DO THIS
+        # maybe have a temp def variable and increase it for the duration of 1 turn and bring it back to 1 after turn ends
+        # problem: having to update temp def before start of next turn
+        print()
+    elif choice == 3:  # 3. Heal
+
+        # Check if there's any potions left & if health isn't max
+        if player.curr_potionCount > 1 and player.curr_HP < player.max_HP:  # if yes, increment health and decrement potion count
+            print(player.name, "'s HP:", player.curr_HP, ", Max HP:", player.max_HP)
+            player.curr_potionCount -= 1
+            if player.curr_HP + potionHealth >= player.max_HP:  # don't let health go above max
+                player.curr_HP = player.max_HP
+            else:
+                player.curr_HP += potionHealth
+            print(player.name, "'s HP after potion:", player.curr_HP)
+        else:  # if not, send to start of menu
+            print(
+                "You have no potions left or your health is already at maximum. Please go to the start of the menu again \n")
+            playerMenu(player, enemy)
+
+    else:
+        print("Invalid Input")
+
+
+# provides choices to enemy during their turn and acts on them
+def enemyMenu(enemy: Enemy):  # IN PROGRESS
+    print("Menu For: ",enemy.name)
+    print("Choose: \n1. Target Move \n2. Sweeping move")
+    choice = int(input("Enter corresponding number: "))
+
+    # LEFT TO DO:
+    # check for guards
+
+    # validating menu input
+    '''if choice != 1 or choice != 2:
+        print("Invalid Input")
+        enemyMenu(enemy)'''
+
+    player = wizard  # temp
+    if choice == 1:  # 1. Target move
+        print("The players in combat are: ")
+        for i in range(0, len(playerChars)):
+            print(i, playerChars[i])
+        target = int(input("Enter number corresponding to player for targeting: "))
+        player = playerChars[target]
+
+    print()
+    # calculating and dealing damage to player
+    dealDMG_E2P(enemy, player, calculateDMG_E2P(enemy, choice, player), choice)
+
+
+# player to enemy damage calculator
+# uses player's attack and dmg along with enemy's defense + some randomisation
+def calculateDMG_P2E(player: PlayerChar, enemy: Enemy) -> int:
+    # random factor
+    randomfac = random.uniform(1, 2)
+
+    # calculate damage
+    dmg = (player.attack * player.move_dmg * randomfac) / enemy.defense
+    return int(dmg)
+
+
+# actually deals damage and updates the enemy's hp
+def dealDMG_P2E(dmg: int, enemy: Enemy):
+    print(enemy.name, "'s HP was: ", enemy.curr_HP)
+    enemy.curr_HP -= dmg
+    print("Damage dealt was: ", dmg)
+    print(enemy.name, "'s HP is now: ", enemy.curr_HP)
+
+
+# enemy to player damage calculator
+# uses enemy's attack and dmg along with player's defense + some randomisation
+def calculateDMG_E2P(enemy: Enemy, move_num: int, player: PlayerChar) -> int:
+    # random factor
+    randomfac = random.uniform(0.1, 0.45)
+
+    #  pick move number (thus dmg) based on move_num arg
+    if move_num == 1:
+        move_dmg = enemy_stats.iat[enemy.enemy_num, 6]  # move 1
+    elif move_num == 2:
+        move_dmg = enemy_stats.iat[enemy.enemy_num, 8]  # move 2
+
+    # calculate damage per player (regardless of move type)
+    dmg = (enemy.attack * move_dmg * randomfac) / player.defense
+    return int(dmg)
+
+
+# actually deals damage and updates the enemy's hp
+def dealDMG_E2P(enemy: Enemy, player: PlayerChar, dmg: int, move_num: int):
+    if (move_num == 1):  # deal damage only to one player
+        print("Move Name: ", enemy.move1_name)
+        print("The player targetted is: ", player.name)
+        print(player.name, "'s HP was: ", player.curr_HP)
+        player.curr_HP -= dmg
+        print("Damage dealt was: ", dmg)
+        print(player.name, "'s HP is now: ", player.curr_HP)
+
+    elif (move_num == 2):  # deal damage to everyone
+        print("Move Name: ", enemy.move2_name)
+        print("This move deals damage to all players")
+        for i in playerChars:
+            print(i.name, "'s HP was: ", i.curr_HP)
+            player.curr_HP -= dmg
+            print("Damage dealt was: ", dmg)
+            print(i.name, "'s HP is now: ", i.curr_HP)
+            print()
+
+
+# === End Of III - Combat Related Functions ===
+
+# IV - Functions Related To Start Of Whole Program
 def initialise():
     # printing stats for all entities in game
     for i in playerChars:
@@ -247,10 +327,10 @@ def initialise():
 
 
 def main():
+    enemyMenu(mimir)
 
     # initialise()
-
-    initialiseCombat(sphinx)
+    # initialiseCombat(sphinx)
 
     # testing P2E functions
     # dealDMG_P2E(calculateDMG_P2E(wizard, sphinx), sphinx)
