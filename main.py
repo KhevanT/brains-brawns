629e4235815d6a3f60d6951272031fce0187193b
Khevan Thanki <Khevanyt@gmail.com>
2023-02-10 23:56:06 +0530

    Added guard move for player and enemy

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import random
 import numpy as np
 import pandas as pd
 
+# TO DO
+# PRETTY PRINT MODULE
+# LOOK FOR OTHER MODULES
+
 # other import settings
 pd.set_option('display.max_columns', None)  # disabling truncation of columns in dataframes
 
@@ -13,8 +17,28 @@ pd.set_option('display.max_columns', None)  # disabling truncation of columns in
 class_stats = pd.read_csv(os.path.join("CSV", "Class_Stats.csv"), header=0, encoding='latin-1')
 enemy_stats = pd.read_csv(os.path.join("CSV", "Enemy_Stats.csv"), header=0, encoding='latin-1')
 
+# I - Logging & Output Functionss
 
-# I - Classes
+# clear log file from previous run of code
+# NOT WORKING
+if os.path.exists('Brains+BrawnsBattleEncounter.txt'):
+    os.remove('Brains+BrawnsBattleEncounter.txt')
+
+
+# display messages in log screen and adds them to text file
+def log_msg(a: str, newline=0):
+    level = logging.INFO
+    format_ = '%(message)s'
+    handlers = [logging.FileHandler('BrainsBrawnsBattleEncounter.txt'), logging.StreamHandler()]
+    logging.basicConfig(level=level, format=format_, handlers=handlers)
+    logging.info(a)
+    if newline == 1:  # default new line is 0
+        logging.info(" ")
+
+
+# === End Of I - Logging & Output Functions ===
+
+# II - Classes
 # Player Character Class
 class PlayerChar:
     def __init__(self, name, class_num: int):
@@ -46,6 +70,11 @@ class PlayerChar:
         self.max_potionCount = class_stats.iat[class_num, 7]
         self.curr_potionCount = self.max_potionCount
 
+        # guard defense
+        # will act as a multiplier for defense if guard is up
+        self.tempDefenseBoost = 2.5
+        self.guardUp = False
+
         # stats dictionary
         stats_keys = ["Name", "Class", "Max HP", "Current HP", "Attack", "Defense", "Speed", "Move Name", "Move Damage",
                       "Potion Count"]
@@ -58,7 +87,7 @@ class PlayerChar:
         return str
 
     def PrintStats(self):
-        print("The stats for " + self.name + " are: \n", self.stats)
+        log_msg("The stats for " + self.name + " are: \n" + self.stats.to_string())
 
 
 class Enemy:
@@ -75,6 +104,11 @@ class Enemy:
         self.defense = enemy_stats.iat[enemy_num, 3]
         self.speed = enemy_stats.iat[enemy_num, 4]
 
+        # guard defense
+        # will act as a multiplier for defense if guard is up
+        self.tempDefenseBoost = 2.5
+        self.guardUp = False
+
         # moves and damage
         self.move1_name = enemy_stats.iat[enemy_num, 5]
         self.move1_dmg = enemy_stats.iat[enemy_num, 6]
@@ -88,12 +122,12 @@ class Enemy:
                         self.move1_name, self.move1_dmg, self.move2_name, self.move2_dmg]
         self.stats = pd.DataFrame(data=[stats_values], columns=stats_keys)
 
-    def __str__(self):  # for print(playerChar)
+    def __str__(self):  # for print (playerChar)
         str = "Name: " + self.name
         return str
 
     def PrintStats(self):
-        print("The stats for " + self.name + " are: \n", self.stats)
+        log_msg("The stats for " + self.name + " are: \n" + self.stats.to_string())
 
 
 # global vars
@@ -116,22 +150,7 @@ athena = Enemy(2)
 enemyChars = [sphinx, mimir, athena]
 
 
-# === End Of I - Classes ===
-
-# II - Logging & Output Functions
-
-# display messages in log screen and adds them to text file
-def log_msg(a: str, newline):
-    level = logging.INFO
-    format_ = '%(message)s'
-    handlers = [logging.FileHandler('Brains+BrawnsBattleEncounter.txt'), logging.StreamHandler()]
-    logging.basicConfig(level=level, format=format_, handlers=handlers)
-    logging.info(a)
-    if newline == 1:
-        logging.info(" ")
-
-
-# === End Of II - Logging & Output Functions ===
+# === End Of II - Classes ===
 
 # III - Combat Related Functions
 
@@ -167,10 +186,10 @@ def initialiseCombat(enemy: Enemy):
             orderOfCombat[i] = max
 
     # print sorted list
-    print("The order of combat is: ")
+    log_msg("The order of combat is: ")
     for i in orderOfCombat:
-        print(i, i.speed)
-    print()
+        log_msg((i.name + ", Speed: " + str(i.speed)))
+    log_msg(" ")
 
     # 2. Restore dead player's health with penalty
     for i in playerChars:
@@ -178,10 +197,10 @@ def initialiseCombat(enemy: Enemy):
             i.curr_HP = int(i.max_HP * 0.3)  # Restored health == 30% of max
 
     # checking if it worked
-    print("The health of players after restoring health is: ")
+    log_msg("The health of players after restoring health is: ")
     for i in orderOfCombat:
-        print(i, ", Current HP:", i.curr_HP, ", Max HP: ", i.max_HP)
-    print()
+        log_msg(str(i.name + ", Current HP:" + str(i.curr_HP) + ", Max HP: " + str(i.max_HP)))
+    log_msg(" ")
 
     # 3. check for weakness code and set damage multiplier
     # <INCOMPLETE>
@@ -189,70 +208,80 @@ def initialiseCombat(enemy: Enemy):
 
 # provides choices to players during their turn and acts on them
 def playerMenu(player: PlayerChar, enemy: Enemy):  # IN PROGRESS
-    print("Menu For: ", player.name)
-    print("Choose: \n1.Attack \n2.Guard \n3.Heal")
+    log_msg("Menu For: " + player.name)
+    log_msg("Choose: \n1.Attack \n2.Guard \n3.Heal")
     choice = int(input("Enter corresponding number: "))
-
+    log_msg("Option Chosen Is: " + str(choice))
     # LEFT TO DO:
-    # check for damage multipliers
+    # check for weakness code damage multipliers
+
+    # get guard down if on previously
+    if player.guardUp == True:
+        player.guardUp = False
+        log_msg(player.name + "'s guard was previously up. It has now been lowered to normal.")
 
     if choice == 1:  # 1. Attack
         # Print move name and dmg
-        print("Move Name: ", player.move_name, ", Move DMG: ", player.move_dmg)
+        log_msg("Move Name: " + player.move_name + ", Move DMG: " + str(player.move_dmg))
 
         # Call for calculation and dealing of damage
         dealDMG_P2E(calculateDMG_P2E(player, enemy), enemy)
 
     elif choice == 2:  # 2. Guard
-        # HOW TF DO I DO THIS
-        # maybe have a temp def variable and increase it for the duration of 1 turn and bring it back to 1 after turn ends
-        # problem: having to update temp def before start of next turn
-        print()
+        player.guardUp = True
+        log_msg(
+            player.name + "'s guard has been raised. If attacked, next before next turn, the damage will be significantly reduced")
     elif choice == 3:  # 3. Heal
 
         # Check if there's any potions left & if health isn't max
         if player.curr_potionCount > 1 and player.curr_HP < player.max_HP:  # if yes, increment health and decrement potion count
-            print(player.name, "'s HP:", player.curr_HP, ", Max HP:", player.max_HP)
+            log_msg((player.name + "'s HP:" + str(player.curr_HP) + ", Max HP:" + str(player.max_HP)))
             player.curr_potionCount -= 1
             if player.curr_HP + potionHealth >= player.max_HP:  # don't let health go above max
                 player.curr_HP = player.max_HP
             else:
                 player.curr_HP += potionHealth
-            print(player.name, "'s HP after potion:", player.curr_HP)
+            log_msg((player.name + "'s HP after potion:" + str(player.curr_HP)))
         else:  # if not, send to start of menu
-            print(
+            log_msg(
                 "You have no potions left or your health is already at maximum. Please go to the start of the menu again \n")
             playerMenu(player, enemy)
 
     else:
-        print("Invalid Input")
+        log_msg("Invalid Input")
 
 
 # provides choices to enemy during their turn and acts on them
 def enemyMenu(enemy: Enemy):  # IN PROGRESS
-    print("Menu For: ", enemy.name)
-    print("Choose: \n1. Target Move \n2. Sweeping move")
+    log_msg("Menu For: " + enemy.name)
+    log_msg("Choose: \n1. Target Move \n2. Sweeping Move \n3. Guard")
     choice = int(input("Enter corresponding number: "))
+    log_msg("Option Chosen Is: " + str(choice))
 
-    # LEFT TO DO:
-    # check for guards
+    # get guard down if on previously
+    if enemy.guardUp == True:
+        enemy.guardUp = False
+        log_msg(enemy.name + "'s guard was previously up. It has now been lowered to normal.")
 
     # validating menu input
-    '''if choice != 1 or choice != 2:
-        print("Invalid Input")
-        enemyMenu(enemy)'''
+    # LEFT
 
     player = wizard  # temp
     if choice == 1:  # 1. Target move
-        print("The players in combat are: ")
+        log_msg("\nThe players in combat are: ")
         for i in range(0, len(playerChars)):
-            print(i, playerChars[i])
+            log_msg(str(str(i) + " " + playerChars[i].name))
         target = int(input("Enter number corresponding to player for targeting: "))
         player = playerChars[target]
 
-    print()
-    # calculating and dealing damage to player
-    dealDMG_E2P(enemy, player, calculateDMG_E2P(enemy, choice, player), choice)
+    if choice == 1 or choice == 2:
+        # calculating and dealing damage to player
+        dealDMG_E2P(enemy, player, calculateDMG_E2P(enemy, choice, player), choice)
+    elif choice == 3:
+        enemy.guardUp = True
+        log_msg(enemy.name + "'s guard has been raised. If attacked, next before next turn, the damage will be significantly reduced")
+        log_msg(" ")
+
 
 
 # player to enemy damage calculator
@@ -268,11 +297,19 @@ def calculateDMG_P2E(player: PlayerChar, enemy: Enemy) -> int:
 
 # actually deals damage and updates the enemy's hp
 def dealDMG_P2E(dmg: int, enemy: Enemy):
-    print(enemy.name, "'s HP was: ", enemy.curr_HP)
-    enemy.curr_HP -= dmg
-    print("Damage dealt was: ", dmg)
-    print(enemy.name, "'s HP is now: ", enemy.curr_HP)
+    log_msg((enemy.name + "'s HP was: " + str(enemy.curr_HP)))
 
+    # reduce damage if guard is up
+    if enemy.guardUp:
+        dmg = dmg / enemy.tempDefenseBoost
+        log_msg(enemy.name + " had their guard up. Their damage taken was reduced.")
+        enemy.guardUp = False
+
+    # decrement health
+    enemy.curr_HP -= dmg
+    log_msg("Damage dealt was: " + str(dmg))
+    log_msg((enemy.name + "'s HP is now: " + str(enemy.curr_HP)))
+    log_msg(" ",1)
 
 # enemy to player damage calculator
 # uses enemy's attack and dmg along with player's defense + some randomisation
@@ -294,22 +331,41 @@ def calculateDMG_E2P(enemy: Enemy, move_num: int, player: PlayerChar) -> int:
 # actually deals damage and updates the enemy's hp
 def dealDMG_E2P(enemy: Enemy, player: PlayerChar, dmg: int, move_num: int):
     if (move_num == 1):  # deal damage only to one player
-        print("Move Name: ", enemy.move1_name)
-        print("The player targetted is: ", player.name)
-        print(player.name, "'s HP was: ", player.curr_HP)
-        player.curr_HP -= dmg
-        print("Damage dealt was: ", dmg)
-        print(player.name, "'s HP is now: ", player.curr_HP)
+        log_msg(str("Move Name: " + enemy.move1_name))
+        log_msg(str("The player targeted is: " + player.name))
+        log_msg(str(player.name + "'s HP was: " + str(player.curr_HP)))
 
-    elif (move_num == 2):  # deal damage to everyone
-        print("Move Name: ", enemy.move2_name)
-        print("This move deals damage to all players")
+        # reduce damage if guard is up
+        if player.guardUp:
+            dmg = int(dmg / player.tempDefenseBoost)
+            log_msg(player.name + " had their guard up. Their damage taken was reduced.")
+            player.guardUp = False
+
+        # decrement health
+        player.curr_HP -= dmg
+
+        log_msg(str("Damage dealt was: " + str(dmg)))
+        log_msg(str(player.name + "'s HP is now: " + str(player.curr_HP)))
+
+    elif move_num == 2:  # deal damage to everyone
+        log_msg(("Move Name: " + enemy.move2_name))
+        log_msg("This move deals damage to all players")
         for i in playerChars:
-            print(i.name, "'s HP was: ", i.curr_HP)
-            player.curr_HP -= dmg
-            print("Damage dealt was: ", dmg)
-            print(i.name, "'s HP is now: ", i.curr_HP)
-            print()
+            log_msg((i.name + "'s HP was: " + str(i.curr_HP)))
+
+            # reduce damage if guard is up
+            if i.guardUp:
+                i.curr_HP -= int(dmg / player.tempDefenseBoost)  # decrement health with reduced damage
+                log_msg(i.name + " had their guard up. Their damage taken was reduced.")
+                i.guardUp = False
+                log_msg(("Damage dealt was: " + str(int(dmg / player.tempDefenseBoost))))
+            else:  # decrement health with normal damage
+                i.curr_HP -= dmg
+                log_msg(("Damage dealt was: " + str(dmg)))
+
+            log_msg((i.name + "'s HP is now: " + str(i.curr_HP)))
+
+    log_msg(" ",1)
 
 
 # === End Of III - Combat Related Functions ===
@@ -317,22 +373,32 @@ def dealDMG_E2P(enemy: Enemy, player: PlayerChar, dmg: int, move_num: int):
 # IV - Functions Related To Start Of Whole Program
 def initialise():
     # printing stats for all entities in game
+    log_msg("The player stats are: ")
     for i in playerChars:
         i.PrintStats()
-        print()
+        log_msg(" ")
+    log_msg("\n")
 
+    log_msg("The stats for the enemies are: ")
     for i in enemyChars:
         i.PrintStats()
-        print()
+        log_msg(" ")
+    log_msg("\n")
 
 
 def main():
+    initialise()
 
-    # initialise()
-    # initialiseCombat(sphinx)
+    initialiseCombat(mimir)
 
-    # playerMenu(wizard, mimir)
-    # enemyMenu(mimir)
+    # testing enemy and player turns
+    for i in range(3):
+        playerMenu(archer, mimir)
+        enemyMenu(mimir)
+        playerMenu(wizard, mimir)
+        playerMenu(knight, mimir)
+
+
 
 # run only the main function if the .py file is run
 if __name__ == "__main__":
